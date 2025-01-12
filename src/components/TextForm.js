58d0f366c552514be262127eef3106bedea48b32
atharv <atharv1002@gmail.com>
2025-01-12 16:59:49 +0530

    video 8

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,17 +6,29 @@ export default function TextForm(props) {
     setText(newtext);
   }
 
+  const handelLowClick = ()=> {
+    let newtext = text.toLowerCase();
+    setText(newtext);
+  } 
   const handelOnChange = (event)=> {
     setText(event.target.value);
     
   }
-  const[text , setText] = useState('Enter Text here');
+  const[text,setText] = useState('');
   return (
     <>
       <div class="mb-3">
         <h1>{props.heading}</h1>
         <textarea class="form-control" value = {text} onChange={handelOnChange} id="myBox" rows="12"></textarea>
-        <button type="button"  class="btn btn-primary my-3" onClick={handelUpClick}>Uppercase</button>
+        <button type="button"  class="btn btn-primary my-3 mx-2" onClick={handelUpClick}>Uppercase</button>
+        <button type="button"  class="btn btn-primary my-3 mx-2" onClick={handelLowClick}>Lowercase</button>
+      </div>
+
+      <div className="container my-2">
+        <h2>Your Text Summary</h2>
+        <p>Word Count: {text.split (" ").length}</p>
+        <p>Character Count: {text.length}</p>
+        <p> <b>Approx Read Time: {0.008*text.split (" ").length} Mins </b></p>
       </div>
     </>
   );
